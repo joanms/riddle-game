19e8afd8683bbee854a14d5096d84503ba7a9da6
Joan Sweeney <joanms@eircom.net>
2018-09-16 10:43:35 +0000

    Added HTML file for gameplay page

--- run.py
+++ run.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def index():
     return render_template("index.html")
+
+@app.route('/play')
+def play():
+    return render_template("play.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
