df2c0aca688241f24f8aea58d11bf075051db890
Joan Sweeney <joanms@eircom.net>
2018-10-04 16:18:56 +0000

    Added code for MagicMock test

--- test_riddler.py
+++ test_riddler.py
@@ -1,7 +1,17 @@
 import unittest
 import riddler
-        
-class TestRiddler(unittest.TestCase):
-    """
-    Test suite for riddle game
-    """
+from mock import mock_open, MagicMock, patch, call
+
+m = mock_open()
+with patch('__main__.open', m, create=True):
+     with open('data/users.txt', 'w') as h:
+         h.write('some stuff')
+
+m.mock_calls
+[call('foo', 'w'),
+call().__enter__(),
+call().write('some stuff'),
+call().__exit__(None, None, None)]
+m.assert_called_once_with('foo', 'w')
+handle = m()
+handle.write.assert_called_once_with('some stuff')    
