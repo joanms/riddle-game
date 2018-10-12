9bc5c8c16b06a15349c5ddcf254f97fca1b60ae4
Joan Sweeney <joanms@eircom.net>
2018-10-12 16:12:36 +0000

    Added experimental code to run.py

--- run.py
+++ run.py
@@ -5,6 +5,27 @@ from flask import Flask, flash, redirect, render_template, request
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
+def get_riddle(index):
+    with open("data/riddles.json", "r") as json_file:
+        riddles = json.loads(json_file)
+        return riddles[index]
+        
+# CHRIS' CODE START
+def init_game(username):
+    score = 0
+    attempt = 1
+    riddle = get_riddle(0)
+    context = {
+        'riddle_index': 0,
+        'riddle': riddle['question'],
+        'answer': riddle['answer'],
+        'username': username,
+        'current_score': score,
+        'attempt': attempt
+    }
+    return context
+# CHRIS' CODE END
+
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -12,10 +33,7 @@ def index():
         if username not in open("data/users.txt").read():
             with open("data/users.txt", "a") as user_list:
                 user_list.writelines(username +"\n")
-            data = []
-            with open("data/riddles.json", "r") as json_file:
-                data = json.load(json_file)
-                return render_template("play.html", page_title="Play the Game!", riddles=data)
+                return render_template("play.html", page_title="Play the Game!")
         else:
             flash("That username is already taken. Please try another one.")
     return render_template("index.html", page_title="Please select a username:")
