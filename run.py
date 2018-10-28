374fe67b0f050e583cf8a76c5a07b44abcebbee3
Joan Sweeney <joanms@eircom.net>
2018-10-28 11:27:02 +0000

    Added code for sessions

--- run.py
+++ run.py
@@ -1,69 +1,56 @@
 import os
 import json
-from flask import Flask, flash, redirect, render_template, request
+from flask import Flask, flash, session, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
 
-# Start of code from my mentor, Chris Zielinski
-
-# Get the info for the next riddle
-def get_riddle(index):
-    with open('data/riddles.json') as json_riddles:
-        riddles = json.loads(json_riddles.read())
-        return riddles[index] if index < 10 else None # Return None to avoid IndexError on the last riddle
-
-# Initialise the game with some default values
-def init_game(username):
-    score = 0
-    attempt = 1
-    riddle = get_riddle(0)
-    context = {
-        'riddle_index': 0,
-        'riddle': riddle['question'],
-        'answer': riddle['answer'],
-        'username': username,
-        'current_score': score,
-        'attempt': attempt
-    }
-    return context
-    
-# End of code from my mentor
-
 # User logs in and the first question is displayed
-@app.route('/', methods=["GET", "POST"])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == "POST":
-        username = request.form["username"]
-        if username not in open("data/users.txt").read():
-            with open("data/users.txt", "a") as user_list:
-                user_list.writelines(username +"\n")
-                context = init_game(username)
-                return render_template("ready.html", context=context, username=username)
-        else:
-            flash("That username is already taken. Please try another one.")
-    return render_template("index.html")
+    if request.method == 'POST':
+        session.pop('user', None)
+        session['user'] = request.form['username']
+        return redirect(url_for('ready'))
+    return render_template('index.html')
+    
+@app.route('/getsession')
+def getsession():
+    if 'user' in session:
+        return session['user']
+        
+    return 'Not logged in'   
+    
+@app.route('/dropsession')
+def dropsession():
+    session.pop('user', None)
+    return 'Dropped'
+    
+@app.route('/ready')
+def ready():
+    return render_template('ready.html')
 
-@app.route('/play/<username>', methods=["GET", "POST"])
+
+@app.route('/play/<username>', methods=['GET', 'POST'])
 def play(username):
     context = init_game(username)
-    if request.method == "POST":
-        riddle_index = 0
+    if request.method == 'POST':
         riddle = get_riddle(riddle_index)
-        user_answer = request.form.get("user_answer")
-        correct_answer = riddle["answer"]
+        user_answer = request.form.get('user_answer')
+        correct_answer = riddle['answer']
         if user_answer != None: # This avoids an error when the user hasn't answered yet.
             correct = user_answer.lower() == correct_answer.lower() # The answer should not be case sensitive
             if correct:
-                flash("Well done!")
+                flash('Well done!')
+                riddle_index += 1
             else:
-                flash("{} was the wrong answer. Please try again.".format(user_answer))
-    return render_template("play.html", context=context, username=username)
+                flash('{} was the wrong answer. Please try again.'.format(user_answer))
+    return render_template('play.html', context=context, username=username)
 
 @app.route('/leaderboard')
 def leaderboard():
-    return render_template("leaderboard.html")
+    return render_template('leaderboard.html')
     
 
 if __name__ == '__main__':
