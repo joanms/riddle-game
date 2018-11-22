bacb8d15afe01492a7d5c42ceba1e3a6f32a183a
Joan Sweeney <joanms@eircom.net>
2018-11-22 15:05:31 +0000

    Added more tests

--- test.py
+++ test.py
@@ -9,7 +9,18 @@ class FlaskTestCase(unittest.TestCase):
                     tester = app.test_client(self)
                     response = tester.get('/', content_type='html/text')
                     self.assertEqual(response.status_code, 200)
-                    
-                    
+          
+          # Ensure that main page requires user login
+          def test_main_route_requires_login(self):
+                    tester = app.test_client(self)
+                    response = tester.get('/', follow_redirects=True)
+                    self.assertIn(b'Please select a username', response.data)
+
+          # Ensure the leaderboard page loads
+          def test_riddles_display(self):
+                    tester = app.test_client(self)
+                    response = tester.get('/leaderboard', follow_redirects=True)
+                    self.assertEqual(response.status_code, 200)
+          
 if  __name__ == '__main__':
           unittest.main()
